d4259431636c9a97efb40eb53da26512b9a9a52d
dabuek <xsz300@gmail.com>
2026-05-04 18:40:44 +0200

    corrected api string for weather api test, respond bad request

--- src/main/kotlin/com/chatting/chatup/controllers/KotlinChatController.kt
+++ src/main/kotlin/com/chatting/chatup/controllers/KotlinChatController.kt
@@ -1,5 +1,6 @@
 package com.chatting.chatup.controllers
 
+import com.chatting.chatup.weather.WeatherService
 import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class KotlinChatController(val aiModel: OllamaChatModel) {
+class KotlinChatController(aiModel: OllamaChatModel, weatherService: WeatherService) {
 
     private val chatModel: OllamaChatModel = aiModel;
+    private val weatherService: WeatherService = weatherService;
 
 
 
@@ -22,5 +24,17 @@ class KotlinChatController(val aiModel: OllamaChatModel) {
     @GetMapping("/ai/chat")
     fun chat(@RequestParam(value = "message") message: String): String? {
         return chatModel.call(message);
+    }
+
+    @GetMapping("/ai/weather")
+    fun getGotherburgWeater(): String? {
+        val rawWeatherData = weatherService.getGothenburgWeather() ?: return "Kunde inte hämta vädret just nu";
+
+        val promt = """
+            Här är rådata om vädret i Göteborg just nu: $rawWeatherData
+            skriv en kort och trevlig sammanfattning på svenska baserat på datan
+        """.trimIndent()
+
+        return chatModel.call(promt);
     }
 }
