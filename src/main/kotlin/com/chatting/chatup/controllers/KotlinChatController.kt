54cff09812dd38831ba20213bffee11155e36102
dabuek <xsz300@gmail.com>
2026-05-05 22:02:18 +0200

    response back from open ai, in system print

--- src/main/kotlin/com/chatting/chatup/controllers/KotlinChatController.kt
+++ src/main/kotlin/com/chatting/chatup/controllers/KotlinChatController.kt
@@ -1,47 +1,36 @@
 package com.chatting.chatup.controllers
 
 import com.chatting.chatup.config.WebService
-import com.chatting.chatup.weather.WeatherService
-import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class KotlinChatController(
-    webService: WebService,
-    aiModel: OllamaChatModel,
-    weatherService: WeatherService) {
+    webService: WebService
+) {
 
     private val webService = webService
 
 
 
     @GetMapping("/")
-    fun index(): String = "chatroom"
+    fun index(@ModelAttribute("chatMessage") message: String): String {
+        return "chatroom"
+    }
 
 
     @PostMapping("/chat")
-    fun postMessage(@ModelAttribute("chatMessage") message: String): String {
+    fun postMessage(
+        @ModelAttribute("chatMessage") message: String,
+        model: Model
+    ): String {
         val response = webService.askAi(message)
-        println(response)
+        //println(response)
+        model.addAttribute("responseMessage", response)
         return "chatroom"
     }
-//    @GetMapping("/ai/chat")
-//    fun chat(@RequestParam(value = "message") message: String): String? {
-//        return chatModel.call(message);
-//    }
-//
-//    @GetMapping("/ai/weather")
-//    fun getGotherburgWeater(): String? {
-//        val rawWeatherData = weatherService.getGothenburgWeather() ?: return "Kunde inte hämta vädret just nu";
-//
-//        val promt = """
-//            Här är rådata om vädret i Göteborg just nu: $rawWeatherData
-//            skriv en kort och trevlig sammanfattning på svenska baserat på datan
-//        """.trimIndent()
-//
-//        return chatModel.call(promt);
-//    }
+
 }
