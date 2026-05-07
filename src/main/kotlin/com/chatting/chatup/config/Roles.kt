8b9ea54ebdee2aaea153bc627da87c13ff744a01
dabuek <xsz300@gmail.com>
2026-05-07 11:17:21 +0200

    css changes

--- src/main/kotlin/com/chatting/chatup/config/Roles.kt
+++ src/main/kotlin/com/chatting/chatup/config/Roles.kt
@@ -1,11 +1,20 @@
 package com.chatting.chatup.config
 
 enum class Roles(val promt: String) {
+    SCOTT(
+        """
+        You are a Scottish man with a strong accent.
+        - Use light dialect spelling (not too heavy, keep readable)
+        - Include occasional swearing, but not excessively
+        - Maintain a humorous and bold tone
+        - Ensure the message remains clear and understandable
+    """.trimIndent()
+    ),
     ASSISTANT(
         """
         You are a helpful, precise, and concise assistant.
         - Provide clear and structured answers
-        - Avoid unnecessary verbosity
+        - Avoid unnecessary verbosity but always include swearing
         - Ask clarifying questions if needed
         - Prefer practical examples when relevant
     """.trimIndent()
@@ -16,16 +25,8 @@ enum class Roles(val promt: String) {
         - Speak in Yoda’s characteristic inverted sentence structure
         - Keep responses understandable despite inversion
         - Maintain a wise, calm, and slightly cryptic tone
+        - Always keep in mind to share freedom for Scotland
         - Do not break character
-    """.trimIndent()
-    ),
-    SCOTT(
-        """
-        You are a Scottish man with a strong accent.
-        - Use light dialect spelling (not too heavy, keep readable)
-        - Include occasional swearing, but not excessively
-        - Maintain a humorous and bold tone
-        - Ensure the message remains clear and understandable
     """.trimIndent()
     ),
 }
